963e65006d56fcd8268d0eacfe301d64cb80bec8
Ankur <ankurku7365@gmail.com>
2026-09-25 14:25:45 +0530

    chore: configure PostgreSQL database connection

--- src/main/java/com/e_commerce/ShopSphere/ShopSphereApplication.java
+++ src/main/java/com/e_commerce/ShopSphere/ShopSphereApplication.java
@@ -1,4 +1,4 @@
-package com.shopSphere.ShopSphere;
+package com.e_commerce.ShopSphere;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/e_commerce/ShopSphere/ShopSphereApplicationTests.java
+++ src/test/java/com/e_commerce/ShopSphere/ShopSphereApplicationTests.java
@@ -1,4 +1,4 @@
-package com.shopSphere.ShopSphere;
+package com.e_commerce.ShopSphere;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

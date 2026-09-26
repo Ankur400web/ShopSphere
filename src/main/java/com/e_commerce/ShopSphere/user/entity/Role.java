2008e7e6f64fa911c40a621704602ef1514ef5a4
Ankur <ankurku7365@gmail.com>
2026-09-26 10:21:17 +0530

    feat: added lombok and created role entity

---
+++ src/main/java/com/e_commerce/ShopSphere/user/entity/Role.java
@@ -0,0 +1,34 @@
+package com.e_commerce.ShopSphere.user.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = OffsetDateTime.now();
+    }
+}

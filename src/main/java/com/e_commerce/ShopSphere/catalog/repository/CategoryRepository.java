d25ae9ae0461ea564a940d3aa456c645708d715c
Ankur <ankurku7365@gmail.com>
2026-09-26 12:51:44 +0530

    zfs

--- src/main/java/com/e_commerce/ShopSphere/catalog/repository/CategoryRepository.java
+++ src/main/java/com/e_commerce/ShopSphere/catalog/repository/CategoryRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.e_commerce.ShopSphere.catalog.entity.Category;
 
-public interface CatalogRepository extends JpaRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }

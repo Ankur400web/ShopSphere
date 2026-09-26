381c8c26303e6742571372765874db0416071c8c
Ankur <ankurku7365@gmail.com>
2026-09-26 11:13:08 +0530

    feat: added INventory entity

---
+++ src/main/java/com/e_commerce/ShopSphere/inventory/Inventory.java
@@ -0,0 +1,53 @@
+package com.e_commerce.ShopSphere.inventory;
+
+import com.e_commerce.ShopSphere.catalog.entity.Product;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "inventory")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(name = "reserved_quantity", nullable = false)
+    private Integer reservedQuantity;
+
+    @Version
+    @Column(nullable = false)
+    private Integer version;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private OffsetDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = OffsetDateTime.now();
+        updatedAt = createdAt;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = OffsetDateTime.now();
+    }
+
+}
---
+++ src/main/java/com/e_commerce/ShopSphere/user/repository/RoleRepository.java
@@ -0,0 +1,4 @@
+package com.e_commerce.ShopSphere.user.repository;
+
+public interface RoleRepository {
+}
